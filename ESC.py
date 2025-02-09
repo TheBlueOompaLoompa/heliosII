1e4237c0e8ae211540bce5d1080b7b4024c204d1
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2025-02-09 00:07:28 -0600

    Add imu,esc,servo,gps

--- ESC.py
+++ ESC.py
@@ -16,10 +16,11 @@ class ESC:
     kit: ServoKit
     _throttle = 0
     
-    def __init__(self, kit: ServoKit, min_value = 700, max_value = 2000):
+    def __init__(self, kit: ServoKit, channel: int, min_value = 700, max_value = 2000):
         self.kit = kit
-        self.kit.continuous_servo[0].set_pulse_width_range(0, max_value)
-        self.kit.continuous_servo[0].throttle = 0
+        self.channel = channel
+        self.kit.continuous_servo[self.channel].set_pulse_width_range(0, max_value)
+        self.throttle = 0
         self.min_value = min_value
         self.max_value = max_value
 
@@ -30,19 +31,19 @@ class ESC:
     @throttle.setter
     def throttle(self, value: float) -> None:
         self._throttle = min(max(value, 0), 1)
-        self.kit.continuous_servo[0].throttle = self._throttle
+        self.kit.continuous_servo[self.channel].throttle = self._throttle
 
     def start_calibration(self):
-        self.kit.continuous_servo[0].throttle = 1
+        self.throttle = 1
         self.state = State.START_CALIBRATION
         print('Starting Calibration')
 
     def finish_calibration(self):
-        self.kit.continuous_servo[0].throttle = 0
+        self.throttle = 0
         self.state = State.CALIBRATING
         sleep(12)
-        self.kit.continuous_servo[0].set_pulse_width_range(0, self.max_value)
-        self.kit.continuous_servo[0].throttle = 0
+        self.kit.continuous_servo[self.channel].set_pulse_width_range(0, self.max_value)
+        self.throttle = 0
         sleep(2)
         self.state = State.STOPPED
 
@@ -51,11 +52,11 @@ class ESC:
     def arm(self):
         if self.state != State.STOPPED:
             return None
-        self.kit.continuous_servo[0].set_pulse_width_range(self.min_value, self.max_value)
+        self.kit.continuous_servo[self.channel].set_pulse_width_range(self.min_value, self.max_value)
         self.state = State.ARMING
-        self.kit.continuous_servo[0].throttle = 0
+        self.throttle = 0
         sleep(1)
-        self.kit.continuous_servo[0].throttle = 0
+        self.throttle = 0
         sleep(5)
         self.state = State.ARMED
 
--- main.py
+++ main.py
@@ -3,17 +3,69 @@ from adafruit_servokit import ServoKit
 import time
 from ESC import ESC
 import math
+import adafruit_mpu6050
+import serial
+from pynmeagps import NMEAReader, NMEAMessage
 
 i2c = board.I2C()
+
 kit = ServoKit(channels=16, i2c=i2c)
-esc = ESC(kit)
+esc = ESC(kit, 0)
 
-print('Arming')
-esc.arm()
-print('Armed')
-max_val = 200
+imu2 = adafruit_mpu6050.MPU6050(i2c) 
 
-for x in range(max_val):
-    esc.throttle = (math.sin(time.time())+1)/2/4
-    time.sleep(.05)
-esc.throttle = 0
+gps_serial = serial.Serial('/dev/ttyS0')
+
+def esc_test():
+    print('Running ESC test')
+    print('Arming')
+    esc.arm()
+    print('Armed')
+    max_val = 200
+
+    for x in range(max_val):
+        esc.throttle = (math.sin(time.time())+1)/2/1
+        time.sleep(.05)
+    esc.throttle = 0
+
+
+def imu_test():
+    rx = 0
+    ry = 0
+    rz = 0
+    print('Running IMU test')
+    while True:
+        print("Acceleration: X:{:.2f}, Y: {:.2f}, Z: {:.2f} m/s^2".format(*imu2.acceleration))
+        print("Gyro X:{:.2f}, Y: {:.2f}, Z: {:.2f} rads/s".format(*imu2.gyro))
+        rx += imu2.gyro[0]/math.pi*180
+        ry += imu2.gyro[1]/math.pi*180
+        rz += imu2.gyro[2]/math.pi*180
+        print(rx, ry , rz)
+        print("")
+        time.sleep(0.5)
+
+
+def gps_serial_test():
+    nmr = NMEAReader(gps_serial)
+    while True:
+        raw_data, parsed_data = nmr.read()
+        if parsed_data is not None:
+            msg: NMEAMessage = parsed_data
+            #print(msg)
+            if msg.msgID == 'GGA' and (not(msg.NS is int) or msg.NS == 0):
+                print("No satellites")
+
+def servo_test(channel: int):
+    print("Running servo test")
+    kit.servo[channel].set_pulse_width_range(500, 2700)
+    for x in range(5):
+        if x % 2 == 0:
+            kit.servo[channel].angle = 0
+        else:
+            kit.servo[channel].angle = 180
+        time.sleep(.5)
+
+#gps_serial_test()
+
+esc_test()
+servo_test(1)
